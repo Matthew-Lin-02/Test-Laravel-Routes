db0f0e023ca2745c54686d05c81b1129309627ea
Matthew-Lin-02 <matthewlin02@gmail.com>
2024-12-30 00:30:54 +1100

    wrong import

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Admin\TaskController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\StatsController;
 
 
